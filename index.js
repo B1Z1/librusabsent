2eeec1bd98b8503d5094ddd5023c92f9b38bfe89
ilyamishkin <iluhamishkin@gmail.com>
2019-10-25 12:12:18 +0200

    Created api

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hej")
+const hidder = document.createElement("div");
+hidder.style.position = "fixed";
+hidder.style.top = 0;
+hidder.style.left = 0;
+hidder.style.width = "100vw";
+hidder.style.height = "100vh";
+hidder.style.backgroundColor = "white";
+hidder.style.zIndex = "99999999999";
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.appendChild(hidder);
+});
+
+window.addEventListener("load", () => {
+  [...document.querySelectorAll(".box")]
+    .filter($el => $el.style.backgroundColor === "rgb(247, 248, 170)")
+    .map($el => {
+      $el.style.backgroundColor = "rgb(247, 248, 250)";
+      $el.querySelector("a").remove();
+    });
+  setTimeout(() => {
+    hidder.remove();
+  }, 100);
+});
